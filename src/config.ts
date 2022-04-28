cd2d8a4635562953d98861359cf5492251171f81
joneubank <joneubank@gmail.com>
2022-04-28 14:12:10 -0400

    Update all files to reflect no more demo data

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,3 @@ export const ES_INDEX = String(process.env.ES_INDEX || "demo_index");
 export const ES_HOSTS = (process.env.ES_HOSTS || "http://localhost:9200").split(
   ","
 );
-export const INCLUDE_DEMO_DATA = process.env.INCLUDE_DEMO_DATA === "true";
-export const INCLUDE_REAL_DATA = process.env.INCLUDE_REAL_DATA === "true";
